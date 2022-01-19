4b12927a427b0d474a2e473153e63a463d265242
Gabriel Oliveira <gabrielkollun@gmail.com>
2022-01-19 15:58:17 +0000

    Contagem de mensagens por operador por dia incompleto

--- log.php
+++ log.php
@@ -2,132 +2,22 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
-use Carbon\Carbon;
-use Src\AnalizeSheet;
 use Src\LogProcessing;
-use Src\CounterTimeMessage;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-// $filename = readline("Nome do arquivo: ");
 
 $logs_dir = __DIR__ . "/logs/";
 $temp_dir =  __DIR__ . "/temp/processed/";
+$destination = __DIR__ . "/tables/";
 
 
 
 LogProcessing::clearBreakLine($logs_dir, $temp_dir);
 
-LogProcessing::analize($temp_dir, getcwd());
+LogProcessing::analize($temp_dir, $destination);
 
 
-exit("Terminamos aqui \n");
+exit(PHP_EOL . "Terminamos aqui" . PHP_EOL);
 
 
-
-
-
-
-$dir = getcwd() . "/logs/";
-
-$files = array_filter(scandir($dir, 1), function($item) {
-    return !is_dir(getcwd() . "/logs/" . $item);
-});
-
-$analize = new AnalizeSheet();
-
-
-$count = 2;
-
-foreach ($files as $file ) {
-    $log_content = file_get_contents("logs/" . $file);
-    $log_name = $file;
-    $total_send = preg_match_all("/Mensagem enviada ao Sigma/", $log_content);
-    $total_refuse = preg_match_all("/Mensagem recusada/", $log_content);
-    $total_message = $total_send + $total_refuse;
-
-    $log_replace = str_replace("][S", " ", str_replace("[]" , " ", str_replace("\n", "", $log_content)));
-    $log_exploded = explode("][", $log_replace);
-
-
-    echo($file . "\n");
-    echo(" Total de mensagens aprovadas : " . $total_send . "\n");
-    echo(" Total de mensagens recusadas : " . $total_refuse . "\n");
-    echo(" TOTAL DE MENSAGENS : " . $total_message . "\n \n");
-
-    $counter_time = new CounterTimeMessage();
-
-    for ($i=0; $i < count($log_exploded); $i+=4) {
-        print_r($log_exploded[$i]. "\n");
-
-        if (preg_match("/Mensagem enviada ao Sigma/", $log_exploded[$i]) || preg_match("/Mensagem recusada/", $log_exploded[$i])){
-
-            try {
-
-                $string_time = str_replace(["[", "]"], "", $log_exploded[$i - 3]);
-                $time = new Carbon($string_time);
-
-            } catch (Exception $e) {
-                var_dump($file);
-            }
-
-            $index = 0;
-            $counter_time->compareTime($time);
-
-        }
-    }
-
-    break;
-
-
-
-
-    // $logs = file_get_contents("logs/" . $file);
-    // echo($file . "\n");
-    // echo(" Total de mensagens aprovadas : " . $total_send . "\n");
-    // echo(" Total de mensagens recusadas : " . $total_refuse . "\n");
-    // echo(" TOTAL DE MENSAGENS : " . $total_message . "\n \n");
-
-    // $counter_time = new CounterTimeMessage();
-
-
-    // foreach ($filelog as $log) {
-    //     $space = str_replace(",", " ", $log);
-    //     $replace = str_replace(["[", "]"],["", ","], $space);
-    //     $parts = explode(",",$replace);
-
-
-    //     if (preg_match("/Mensagem enviada ao Sigma/", $log) || preg_match("/Mensagem recusada/", $log)){
-
-    //         if (! is_array($parts) || count($parts) < 4) continue;
-
-
-    //         try {
-    //             $time = new Carbon($parts[0]);
-
-    //         } catch (Exception $e) {
-    //             var_dump($file);
-    //             var_dump($log);
-    //             var_dump(substr($parts[0], 1));
-    //         }
-
-            
-    //         $counter_time->compareTime($time);
-
-    //     }
-
-    // }    
-
-    // var_dump($counter_time->getInterval());
-
-    // break;
-
-    $analize->writeCounter($count, $total_send, $total_refuse, $total_message, $log_name, $counter_time->getInterval());
-
-    $count ++ ;
-}
-
-
-$writer = new Xlsx($analize->getSpreadsheet());
-$writer->save($filename .'.xlsx');
 
 ?>
--- src/LogProcessing.php
+++ src/LogProcessing.php
@@ -4,16 +4,24 @@ namespace Src;
 
 use Exception;
 use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
 class LogProcessing
 {
+
     public static function clearBreakLine(string $log_folder, string $temp_folder): void
     {
-        $files = self::onlyFiles($log_folder);
+        $logs = self::onlyFiles($log_folder);
+        $temp = self::onlyFiles($temp_folder);
 
-        foreach ($files as $file) {
+        $logs = array_filter($logs, function($log) use ($temp) {
+            return !in_array($log, $temp);
+         });
+
+
+        foreach ($logs as $file) {
             echo "Tratando o arquivo: " . $file . PHP_EOL;
             $content = file_get_contents($log_folder . $file);
             $replaced = preg_replace(["/[A-Z0-9.\-\_\s]{1}[\n]+/i", "/[\n]+[A-Z0-9.\-\_\s]{1}/i"], " ", $content);
@@ -21,10 +29,43 @@ class LogProcessing
             fwrite($result,$replaced);
             fclose($result);        
         }
-        echo "Tratamentos dos arquivos finalizado!" . PHP_EOL;
+        echo PHP_EOL . "Tratamentos dos arquivos finalizado!" . PHP_EOL;
     }
 
-    public static function analize(string $processed_folder, string $destination)
+    public static function analize(string $processed_folder, string $destination):void
+    {
+  
+        $analize = self::scanFiles($processed_folder);
+
+        $writer = new Xlsx($analize->getSpreadsheet());
+        $extension = '.xlsx';
+        $file_name = 'Analise';
+    
+        $exists_file = in_array($file_name . $extension,scandir($destination));
+
+        if ($exists_file) {
+            for ($i=1;;$i++) {
+                $new_file_name = $file_name .'('.$i.')'.$extension;
+                if(! in_array($new_file_name,scandir($destination))) {
+                    $writer->save($destination . $new_file_name);
+                    break;
+                }
+            }
+        } else {
+            $path_name = $destination . $file_name . $extension;
+            $writer->save($path_name);
+        }
+
+    }
+
+    public static function onlyFiles($folder):array
+    {
+       return array_filter(scandir($folder, 1), function($item) {
+            return ! is_dir($item);
+        });
+    }
+
+    private static function scanFiles(string $processed_folder) : AnalizeSheet
     {
         $regex_send = "/Mensagem enviada ao Sigma/";
         $regex_refuse = "/Mensagem recusada:/";
@@ -49,44 +90,69 @@ class LogProcessing
 
             $counter_time = new CounterTimeMessage();
 
+            $operators = [];
             foreach ($logs_file as $log) {
                 $space = str_replace(",", " ", $log);
                 $replace = str_replace(["[", "]"],["", ","], $space);
                 $parts = explode(",",$replace);
 
                 if (preg_match($regex_send, $log) || preg_match($regex_refuse, $log)){
-                    // if (! is_array($parts) || count($parts) < 4) continue;
                     try {
                         $time = new Carbon($parts[0]);
-
                     } catch (Exception $e) {
                         var_dump($file);
                         var_dump($log);
                         var_dump(substr($parts[0], 1));
                         exit(PHP_EOL . " SCRIPT INTERROMPIDO! " . PHP_EOL . $e);
                     }
-
                     $counter_time->compareTime($time);
 
-                }
+                    if (! array_key_exists($parts[2], $operators)) {
+                        $operators[$parts[2]] = [$time];
+                    } else {
+                       array_push($operators[$parts[2]], $time);
+                    }
+                    
+                }           
 
             }
 
+            $final_statistc = [];
+            $start_daylight = new Carbon("10:00:00");
+            $end_daylight = new Carbon("22:00:00");
+            foreach($operators as $operator => $times) {
+                foreach ($times as $time) {
+                    if ($time->betweenIncluded($start_daylight, $end_daylight)){
+                        if  (! array_key_exists($operator, $final_statistc)){
+                            $final_statistc[$operator] = ["D" => 1];
+                        } else {
+                            array_key_exists("D",$final_statistc[$operator]) ? $final_statistc[$operator]["D"]++ : $final_statistc[$operator] = ["D" => 1] ;
+                        }
+                    } elseif ($time->lessThan($start_daylight)) {
+
+                        if  (! array_key_exists($operator, $final_statistc)){
+                            $final_statistc[$operator] = ["SN" => 1];
+                        } else {
+                            array_key_exists("SN",$final_statistc[$operator]) ? $final_statistc[$operator]["SN"]++ : $final_statistc[$operator] = ["SN" => 1] ;
+                        }
+                    } elseif ($time->greaterThan($start_daylight)) {
+
+                        if  (! array_key_exists($operator, $final_statistc)){
+                            
+                        } else {
+                            array_key_exists("EN",$final_statistc[$operator]) ? $final_statistc[$operator]["EN"]++ : $final_statistc[$operator] = ["EN" => 1] ;
+                        }
+                    } 
+                }
+            }
+            var_dump($final_statistc);
+            exit();
 
             $analize->writeCounter($count, $total_send, $total_refuse, $total_message, str_replace(".log", "",$file), $counter_time->getInterval());
 
             $count ++ ;
         }
 
-
-        $writer = new Xlsx($analize->getSpreadsheet());
-        $writer->save('Analise.xlsx');
-    }
-
-    public static function onlyFiles($folder)
-    {
-       return array_filter(scandir($folder, 1), function($item) {
-            return ! is_dir($item);
-        });
+        return $analize;
     }
 }
